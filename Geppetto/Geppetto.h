0f91bcdf0d39c51915f68b816469c2e8bda7dc97
Jinseo Yoon <rinndash@gmail.com>
2019-05-06 13:34:09 +0900

    Initial Commit

---
+++ Geppetto/Geppetto.h
@@ -0,0 +1,19 @@
+//
+//  Geppetto.h
+//  Geppetto
+//
+//  Created by JinSeo Yoon on 06/05/2019.
+//  Copyright © 2019 rinndash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geppetto.
+FOUNDATION_EXPORT double GeppettoVersionNumber;
+
+//! Project version string for Geppetto.
+FOUNDATION_EXPORT const unsigned char GeppettoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geppetto/PublicHeader.h>
+
+
