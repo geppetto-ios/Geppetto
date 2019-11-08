c5a93482a268db9dae12c83c52212b4d910ecb61
Jinseo Yoon <rinndash@gmail.com>
2019-11-08 12:12:27 +0900

    :sparkles: add SimpleAdder example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import Geppetto
+import RxSwift
+import RxSwiftExt
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+enum Adder: BeginnerProgram {
+    enum Message {
+        case updateLeftOperand(String?)
+        case updateRightOperand(String?)
     }
+    
+    struct Model: ModelType, Copyable {
+        var leftOperand: Int?
+        var rightOperand: Int?
+        
+        var result: Int? { return leftOperand.flatMap { x in rightOperand.map { y in x + y } } }
+        
+        static var initial: Model {
+            return Model(
+                leftOperand: nil, 
+                rightOperand: nil
+            )
+        }
+    }
+    
+    static func update(model: Model, message: Message) -> Model {
+        switch message {
+        case let .updateLeftOperand(left):
+            return model.copy {
+                $0.leftOperand = left.flatMap(Int.init)
+            }
+            
+        case let .updateRightOperand(right):
+            return model.copy {
+                $0.rightOperand = right.flatMap(Int.init)
+            }
+        }
+    }
+    
+    typealias ViewModel = String?
+    
+    static func view(model: Model) -> ViewModel {
+        return model.resultText
+    }
+}
 
+extension Adder.Model {
+    var resultText: String? {
+        return result?.description
+    }
+}
 
+class AdderViewController: ViewController<Adder> {
+    @IBOutlet weak var leftOperandTextField: UITextField!
+    @IBOutlet weak var rightOperandTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func behavior() {
+        super.behavior()
+        
+        let updateLeftOperand$ = leftOperandTextField.rx.value
+            .distinctUntilChanged()
+            .map(Message.updateLeftOperand)
+        
+        let updateRightOperand$ = rightOperandTextField.rx.value
+            .distinctUntilChanged()
+            .map(Message.updateRightOperand)
+        
+        let input$ = Observable.merge(updateLeftOperand$, updateRightOperand$)            
+                    
+        input$
+            .bind(to: rx.dispatch)
+            .disposed(by: disposeBag)
+        
+        rx.updated
+            .map { $0.resultText }
+            .bind(to: resultLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
 
--- Podfile
+++ Podfile
@@ -1,7 +1,15 @@
 use_frameworks!
 
-target 'Geppetto' do
-    pod 'RxSwift', '~> 5'
+def rx_dependencies
+	pod 'RxSwift', '~> 5'
     pod 'RxCocoa', '~> 5'
     pod 'RxSwiftExt', '~> 5'
+end
+
+target 'Geppetto' do
+    rx_dependencies
+end
+
+target 'Example' do
+    rx_dependencies
 end
